183b0468765b0677fe880bf80666afd34e7c3cc9
Eric Kufta <erickufta@Erics-MacBook-Pro.local>
2024-03-08 10:19:08 -0800

    staging

--- app.ts
+++ app.ts
@@ -2,7 +2,8 @@
  * Copyright (c) 2014-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
-// adding a comment here
+// Comment for commit
+
 require('./lib/startup/validateDependencies')().then(() => {
   const server = require('./server')
   server.start()
